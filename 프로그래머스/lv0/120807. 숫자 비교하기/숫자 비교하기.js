246c2401e840fc245c0e047e4ba9c850621c006c
박수민 <112373057+suminpark123@users.noreply.github.com>
2023-04-04 20:45:14 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,11 @@
+function solution(num1, num2) {
+    if(num1>=0&&num1<=10000&&num2>=0&&num2<=10000){
+        if(num1==num2){            
+            return 1
+        }
+        else {            
+            return -1
+        }
+    }
+    
+}
