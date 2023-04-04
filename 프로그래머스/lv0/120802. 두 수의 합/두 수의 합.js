9898e8c66f826f0933ddd9d79ba4e34ccda3314b
박수민 <112373057+suminpark123@users.noreply.github.com>
2023-04-04 17:16:16 +0900

    Update 두 수의 합.js

--- 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.js
@@ -1,5 +1,7 @@
 function solution(num1, num2) {
-    if(num1>=-50000 && num1<=500000 && num2>=-50000 && num2<=500000 ){}
-    var answer = num1+num2
+    if(num1>=-50000 && num1<=500000 && num2>=-50000 && num2<=500000 ){
+        var answer = num1+num2
     return answer;
+    }
+    
 }
